f9ec6b1056bca7598caff31d8d39cddabc8054f4
Jiaqi Dong <jd3418@columbia.edu>
2018-04-24 00:16:35 -0400

    update

--- lib/svm.R
+++ lib/svm.R
@@ -1,12 +1,17 @@
+################################----Train the model with svm----##############################
 SVM <- function(train,test){
-  time1 <- Sys.time()
+  # Input: train--training data
+  #        test--test data
+  
+  # Step 0: library packages
   library(e1071)
   library(dplyr)
-  
+  time1 <- Sys.time()
+  # Step 1: train the model with training data 
   model_svm <- svm(x = train[,-1], y = train[,1],   
                  kernel="radial", scale = F, type = "C-classification")
   
-  # Prediction
+  # Step 2:Prediction
   prediction_svm <- predict(model_svm,test[,-1])
   time2 <- Sys.time()
   return(list(prediction = prediction_svm,time = time2 - time1))
