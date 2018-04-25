f8e66cbfd069da840bd520c2d4316f37da08156b
XiaochenFan01 <35549355+XiaochenFan01@users.noreply.github.com>
2018-04-25 11:10:08 -0400

    update

--- lib/gbmp.R
+++ lib/gbmp.R
@@ -11,7 +11,7 @@ gbmp <- function(df.train, df.test){
   traindata <- df.train
   testdata <- df.test
   
-  library(gbm)
+  # library(gbm)
   
   # Step 1: use training data to do the cross validation and train the model
   n<-names(traindata)
@@ -32,6 +32,8 @@ gbmp <- function(df.train, df.test){
   # In order to find the best number of trees to use for the prediction for the test data, we can use `gbm.perf` function. 
   # This function returns the optimal number of trees for prediction.
   optimalTreeNumberPredictionCV = gbm.perf(gbmCV)
+  # Here is optimal number of trees selected by cv is 197 for df.train,
+  # For df.train2, it is 104
   
   # Step 2: prediction
   gbmTest = predict(object = gbmCV,
